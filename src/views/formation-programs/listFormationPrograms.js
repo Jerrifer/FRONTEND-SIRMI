79dcdfc59061bee1f1c5194eb280aaf5dfc2171d
Jerrifer <jerrymeneses27@gmail.com>
2023-04-12 09:30:35 -0500

    Ajustes en importaciones sin usar y cambiando estilos botón

--- src/views/formation-programs/listFormationPrograms.js
+++ src/views/formation-programs/listFormationPrograms.js
@@ -11,9 +11,7 @@ import {
   Table,
   Container,
   Row,
-  Col,
-  NavLink,
-  DropdownToggle,
+  Col
 } from "reactstrap";
 // core components
 import Header from "components/Headers/Header.js";
@@ -108,11 +106,9 @@ const FormationPrograms = () => {
                   tag={NavLinkRRD}
                   activeclassname="active"
                 >
-                  <button class="cssbuttons-io-button"> Registrar
-                    <div class="icon">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
-                    </div>
-                  </button>
+                  <Button className="btn btn-success bg-success">
+                    Registrar
+                  </Button>
                 </Link>
                 
                 </Col>
@@ -168,9 +164,19 @@ const FormationPrograms = () => {
                               formationProgram={formationProgram}
                             />
 
-                            <AssignCompetences
+                            {/* <AssignCompetences
                               formationProgram={formationProgram}
-                            />
+                            /> */}
+                            <Link
+                              to={`/admin/programcompetences/${formationProgram._id}`}
+                              tag={NavLinkRRD}
+                              activeclassname="active"
+                            >
+                              <Button
+                                variant="">
+                                  <i className="fas fa-address-book"></i>
+                              </Button>
+                            </Link>
 
                             <Link
                               to={`/admin/updateformationprograms/${formationProgram._id}`}
