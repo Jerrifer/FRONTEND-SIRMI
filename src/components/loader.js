5610991f3b114ccab459b679601471fc08368960
20jota02 <juananacona04@gmail.com>
2023-04-17 17:04:58 -0500

    cambios en loaderv3

--- src/components/loader.js
+++ src/components/loader.js
@@ -16,12 +16,6 @@ import './loader.css'
 
 </div>
 
-// <div class="circle"></div>
-// <div class="circle"></div>
-// <div class="circle"></div>
-// <div class="shadow"></div>
-// <div class="shadow"></div>
-// <div class="shadow"></div>
 
   )
 }
